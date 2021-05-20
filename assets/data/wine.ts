c307724abec5e8fbab62c1533daba7062845a6e7
Stefaniya1324 <44589645+Stefaniya1324@users.noreply.github.com>
2021-05-20 17:31:17 -0400

    Add files via upload

---
+++ assets/data/wine.ts
@@ -0,0 +1,332 @@
+export default {
+    items: [
+    //bottles 
+    //category1
+    {
+    id: 'category1Wine1',
+    title: 'BIN NUN RESERVE',
+    origin: 'Judean Hills, Izrael',
+    year: 2015, 
+    rating: 4.3,
+    
+    more: {
+        items: [{
+            id: 'category1Wine1',
+            name: 'Reserve Cabernet Sauvignon',
+            front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+            back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+            sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/01/BN_Reserve2016_USA.png',
+            }]
+    }},
+
+    {
+    id: 'category1Wine2',
+    title: 'BIN NUN CUVEE',
+    origin: 'Judean Hills, Izrael',
+    year: 2017, 
+    rating: 3.9,
+        
+    more: {
+        items: [{
+            id: 'category1Wine2',
+            name: 'Reserve Cabernet Sauvignon',
+            front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+            back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+            sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/Bin-Nun-Cuvee-2017.png',
+                }]
+    }},
+    
+    //category2
+    {
+    id: 'category2Wine1',
+    title: 'BORGO REALE PINOT NOIR',
+    origin: 'Puglia, Italy',
+    year: 2019, 
+    rating: 3.2,
+                
+    more: {
+        items: [{
+            id: 'category2Wine1',
+            name: 'Cantine del Borgo Reale',
+            front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+            back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+            sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+            poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Borgo-Reale-Pinot-Noir-2016-bottle-shot.png',
+                }]
+    }},
+    {
+        id: 'category2Wine2',
+        title: 'BORGO REALE ROSE',
+        origin: 'Puglia, Italy',
+        year: 2018, 
+        rating: 3.7,
+        
+        
+        more: {
+            items: [{
+                id: 'category2Wine2',
+                name: 'Cantine del Borgo Reale',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Rose.png',
+                }]
+        }},
+
+    {
+        id: 'category2Wine3',
+        title: 'BORGO REALE MONTEPULCIANO DABRUZZO',
+        origin: 'Puglia, Italy',
+        year: 2019, 
+        rating: 3.5,
+        
+        
+        more: {
+            items: [{
+                id: 'category2Wine3',
+                name: 'Cantine del Borgo Reale',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Montepulciano-Dabruzzo.png',
+                }]
+        }},
+
+    {
+        id: 'category2Wine4',
+        title: 'BORGO REALE PINOT GRIGIO',
+        origin: 'Puglia, Italy',
+        year: 2013, 
+        rating: 3.4,
+        
+        
+        more: {
+            items: [{
+                id: 'category2Wine4',
+                name: 'Pinot Grigio Puglia',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Pinot-Grigio-2013.png',
+                }]
+        }},
+    
+    //category3
+    {
+        id: 'category3Wine1',
+        title: 'DALTON ALMA ROSE',
+        origin: 'Galilee, Israel',
+        year: 2018, 
+        rating: 3.7,
+        
+        
+        more: {
+            items: [{
+                id: 'category3Wine1',
+                name: 'DALTON ALMA ROSE',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Alma-Rose.png',
+                }]
+        }},
+    {
+        id: 'category3Wine2',
+        title: 'DALTON ALMA DEEP RED',
+        origin: 'Galilee, Israel',
+        year: 2018, 
+        rating: 3.7,
+            
+            
+        more: {
+            items: [{
+                id: 'category3Wine2',
+                name: 'DALTON ALMA DEEP RED',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/Wine-Alma-Red.png',
+                }]
+        }},
+    {
+        id: 'category3Wine3',
+        title: 'DALTON RESERVE SHIRAZ',
+        origin: 'Galilee, Israel',
+        year: 2013, 
+        rating: 4.1,
+                
+                
+        more: {
+            items: [{
+                id: 'category3Wine3',
+                name: 'DALTON RESERVE SHIRAZ',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2017/09/Dalton-Reserve-Shiraz-2013-Img.png',
+                }]
+        }},
+    {
+        id: 'category3Wine4',
+        title: 'DALTON RESERVE SHIRAZ',
+        origin: 'Galilee, Israel',
+        year: 2013, 
+        rating: 3.6,
+        
+        
+        more: {
+            items: [{
+                id: 'category3Wine4',
+                name: 'Pinot Grigio Puglia',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/alma_ivory_2017.png',
+                }]
+    }},
+
+//category4
+        {
+        id: 'category4Wine1',
+        title: 'GOFNA CHARDONNAY CABERNET SAUVIGNON',
+        origin: 'Judean, Israel',
+        year: 2019, 
+        rating: 4.0,
+                    
+        more: {
+            items: [{
+                id: 'category4Wine1',
+                name: 'GOFNA CHARDONNAY CABERNET SAUVIGNON',
+                front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Gofna-Chardonnay-Cab-Sauv-New.png',
+                    }]
+        }},
+        {
+            id: 'category4Wine2',
+            title: 'Gofna Petit Verdot',
+            origin: 'Judean, Israel',
+            year: 2018, 
+            rating: 3.7,
+            
+            
+            more: {
+                items: [{
+                    id: 'category4Wine2',
+                    name: 'Gofna Petit Verdot',
+                    front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                    back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                    sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                    poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Gofna-Petit-Verdot.png',
+                    }]
+            }},
+
+        {
+            id: 'category4Wine3',
+            title: 'Gofna Pinot Noir',
+            origin: 'Judean, Israel',
+            year: 2019, 
+            rating: 3.5,
+            
+            
+            more: {
+                items: [{
+                    id: 'category4Wine3',
+                    name: 'Gofna Pinot Noir',
+                    front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                    back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                    sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                    poster: 'http://alliedimporters.com/wp-content/uploads/2016/03/Gofna-Pinot-Noir-NEW.png',
+                    }]
+    }},
+        //category5
+        {
+            id: 'category5Spirit1',
+            title: 'Bottled in Bond',
+            origin: 'Washington, USA', 
+            rating: 4.5,
+            
+            more: {
+                items: [{
+                    id: 'category5Spirit1',
+                    name: 'Bottled in Bond',
+                    front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                    back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                    sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                    poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Bottled-Transparent.png',
+                    }]
+            }},
+        
+            {
+            id: 'category5Spirit2',
+            title: 'Straight Bourbon Whiskey',
+            origin: 'Washington, USA', 
+            rating: 4.5,
+                
+            more: {
+                items: [{
+                    id: 'category5Spirit2',
+                    name: 'Straight Bourbon Whiskey',
+                    front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                    back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                    sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                    poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Transparent.png',
+                        }]
+        }},
+        //category6
+            {
+            id: 'category6Spirit1',
+            title: 'Juke Joint Whiskey',
+            origin: 'Tennessee, USA', 
+            rating: 5.0,
+            
+            more: {
+                items: [{
+                    id: 'category6Spirit1',
+                    name: 'Juke Joint Whiskey',
+                    front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                    back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                    sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                    poster: 'http://alliedimporters.com/wp-content/uploads/2021/03/Juke-Joint.png',
+                    }]
+            }},
+        
+            {
+            id: 'category6Spirit2',
+            title: 'Premium Small Batch Whiskey',
+            origin: 'Tennessee, USA', 
+            rating: 4.7,
+                
+            more: {
+                items: [{
+                    id: 'category6Spirit2',
+                    name: 'Premium Small Batch Whiskey',
+                    front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                    back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                    sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                    poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/Single-Barrel.png',
+                        }]
+            }},
+            {
+            id: 'category6Spirit3',
+            title: 'Riverset Whiskey',
+            origin: 'Tennessee, USA', 
+            rating: 4.5,
+                
+            more: {
+                items: [{
+                    id: 'category6Spirit3',
+                    name: 'Riverset Whiskey',
+                    front: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-f-label.jpg',
+                    back: 'http://alliedimporters.com/wp-content/uploads/2018/02/Borgo-Reale-Chianti-riserva-b-label.jpg',
+                    sell: 'http://alliedimporters.com/wp-content/uploads/2016/03/Borgo-Reale-Chianti-Riserva.pdf',
+                    poster: 'http://alliedimporters.com/wp-content/uploads/2018/02/Riverset.png',
+                        }]
+            }},
+
+    
+    ]
+}
